b325f1377a34dc6f8fcdc745d26c8edff182021d
Juan José Calderón <juancalderon721@outlook.com>
2022-12-02 03:50:08 -0500

    Changed starter items url's

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,16 +25,16 @@ VALUES('d45bb1c6-354f-4052-9381-009679b8f293','jose@icesi.edu.co','Jose.1','Call
 
 --BASE ITEMS
 INSERT INTO item (item_id, name, description, price, url)
-VALUES('d45bb1c6-354f-4052-9381-009679b8f301', 'Toyota Prado TXL', 'Rines y llantas Mickey Thompson 35" Relaciones 4.88 Bumbpers Ultima generacion. LIFT 2". Cadena reparticion recien cambiada. Cambio de aceite y mantenimiento de suspencion recien hecho. Color exclusivo', 340000000,'https://www.img2link.com/image/toyotaTxl.shxa');
+VALUES('d45bb1c6-354f-4052-9381-009679b8f301', 'Toyota Prado TXL', 'Rines y llantas Mickey Thompson 35" Relaciones 4.88 Bumbpers Ultima generacion. LIFT 2". Cadena reparticion recien cambiada. Cambio de aceite y mantenimiento de suspencion recien hecho. Color exclusivo', 340000000,'https://http2.mlstatic.com/D_NQ_NP_770780-MCO52539032838_112022-O.webp');
 
 INSERT INTO item (item_id, name, description, price, url)
-VALUES('d45bb1c6-354f-4052-9381-009679b8f302', 'Ford Raptor 150', 'Modelo 2013. Color Gris. Automatica. 170.000 kms', 98000000,'https://www.img2link.com/image/fortRaptor150.sln4');
+VALUES('d45bb1c6-354f-4052-9381-009679b8f302', 'Ford Raptor 150', 'Modelo 2013. Color Gris. Automatica. 170.000 kms', 98000000,'https://http2.mlstatic.com/D_NQ_NP_958270-MCO50877015841_072022-O.webp');
 
 INSERT INTO item (item_id, name, description, price, url)
-VALUES('d45bb1c6-354f-4052-9381-009679b8f303', 'Volkswagen Golf Gti', 'Modelo 2015. Automatico. 101.760 kms. Papeles al dia y venta directa', 85000000, 'https://www.img2link.com/image/volkswagenGolfGti.siiX');
+VALUES('d45bb1c6-354f-4052-9381-009679b8f303', 'Volkswagen Golf Gti', 'Modelo 2015. Automatico. 101.760 kms. Papeles al dia y venta directa', 85000000, 'https://http2.mlstatic.com/D_NQ_NP_629748-MCO51841630851_102022-O.webp');
 
 INSERT INTO item (item_id, name, description, price, url)
-VALUES('d45bb1c6-354f-4052-9381-009679b8f304', 'Porsche 911 Carrera S', 'Modelo 2018. Automatico. 18.000 kms. Placa impar, terminada en 1', 650000000,'https://www.img2link.com/image/porsche911Carrera.sNt1');
+VALUES('d45bb1c6-354f-4052-9381-009679b8f304', 'Porsche 911 Carrera S', 'Modelo 2018. Automatico. 18.000 kms. Placa impar, terminada en 1', 650000000,'https://http2.mlstatic.com/D_NQ_NP_824530-MCO48437980977_122021-O.webp');
 
 INSERT INTO item (item_id, name, description, price, url)
-VALUES('d45bb1c6-354f-4052-9381-009679b8f305', 'Volkswagen Tiguan', 'Comfortline Modelo 2020. Automatica. 25.000 kms. Papeles al dia y perfecto estado. Vehiculo verificado por AutoMas Comercial Ltda', 123000000,'https://www.img2link.com/image/volkswagenTiguan.sL8N');
+VALUES('d45bb1c6-354f-4052-9381-009679b8f305', 'Volkswagen Tiguan', 'Comfortline Modelo 2020. Automatica. 25.000 kms. Papeles al dia y perfecto estado. Vehiculo verificado por AutoMas Comercial Ltda', 123000000,'https://http2.mlstatic.com/D_NQ_NP_907742-MCO52084333169_102022-O.webp');
